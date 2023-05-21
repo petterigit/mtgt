d9961fd173302a060f9e0ea7e73c5c44478d82eb
petterigit <p-makela@outlook.com>
2023-05-21 10:05:03 +0300

    asset prefix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
     reactStrictMode: true,
     transpilePackages: ['ui'],
     output: 'export',
+
+    // In production, app is served under /mtgt path
+    assetPrefix: isProd ? '/mtgt' : undefined,
 };
