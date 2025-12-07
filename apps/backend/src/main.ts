6b14b6198c0846baf44ebf1184b04cb881801ce6
vilperi <p-makela@outlook.com>
2025-12-07 12:10:32 +0200

    Add environment variables

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
-  app.enableCors({ origin: '*' });
+  const port = configService.get<number>('PORT', 5000);
+  const wsPort = configService.get<number>('WS_PORT', 5080);
+  const corsOrigin = configService.get<string>('CORS_ORIGIN', '*');
 
-  await app.listen('5000');
+  app.enableCors({ origin: corsOrigin });
+
+  await app.listen(port);
 }
 bootstrap();
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,8 @@ module.exports = {
     reactStrictMode: true,
     transpilePackages: ['ui'],
     output: 'standalone',
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+    },
 };
